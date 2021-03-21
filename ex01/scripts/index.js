3173f65429aa07be9f1e143c3685d87e54bbea47
aanc1122 <amelanuhanovic997@gmail.com>
2021-03-22 00:54:01 +0100

    project uploaded

---
+++ ex01/scripts/index.js
@@ -0,0 +1,65 @@
+function loadImage1() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image01.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage2() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image02.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage3() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image03.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage4() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image04.html" ></object>';
+    modal.style.display = "block";
+  }
+  
+  function loadImage5() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image05.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage6() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image06.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage7() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image07.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage8() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image08.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage9() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image09.html" ></object>';
+    modal.style.display = "block";
+  }
+
+  function loadImage10() {
+    var modal = document.getElementById("myModal");
+    document.getElementById("myModal").innerHTML='<object type="text/html" data="C:/Users/Arena/Day13-14/ex01/src/image10.html" ></object>';
+    modal.style.display = "block";
+  }
+
+
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
